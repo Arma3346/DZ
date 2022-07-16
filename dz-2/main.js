50d976e22a59756b745b05560bee92a40469b085
Arma3346 <60913959+Arma3346@users.noreply.github.com>
2022-07-16 11:52:44 +0600

    Add files via upload

--- dz-2/main.js
+++ dz-2/main.js
@@ -1,19 +1,43 @@
-let arr = "*";
-while (arr.length <= 7) {
-     console.log(arr);
-arr += "*";
-}
-const numbers = [];
+// let arr = "*";
+// while (arr.length <= 7) {
+//      console.log(arr);
+// arr += "*";
+// }
+// const numbers = [];
+//
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 15 === 0) {
+//         numbers.push("FizzBuzz");
+//     } else if (i % 3 === 0) {
+//         numbers.push("Fizz");
+//     } else if (i % 5 === 0) {
+//         numbers.push("Buzz");
+//     } else {
+//         numbers.push(i);
+//     }
+// }
+// console.log(numbers);
+//
+// function numbers(number1, number2) {
+//     if (number1 > number2) {
+//         console.log("Первое больше")
+//     } else if (number1 < number2) {
+//         console.log("Второе больше")
+//     } else {
+//         console.log("Они равны")
+//     }
+// }
+// const number = {
+//     number1: prompt("Число?"),
+//     number2: prompt("Второе число?")
+// }
+// numbers(number.number1, number.number2)
+//
+//
 
-for (let i = 1; i <= 100; i++) {
-    if (i % 15 === 0) {
-        numbers.push("FizzBuzz");
-    } else if (i % 3 === 0) {
-        numbers.push("Fizz");
-    } else if (i % 5 === 0) {
-        numbers.push("Buzz");
-    } else {
-        numbers.push(i);
-    }
-}
-console.log(numbers);
+// function dev(main) {
+//     console.log(main.length)
+// }
+// const main = [23, 23, 4]
+//
+// dev(main)
